f6d76acb59edeb41354565fd83c3235854aac76c
marutsingh <marut.singh@delhivery.com>
2016-06-03 21:48:04 +0530

    separate package for candidate

--- src/main/java/candidate/Class1Exercise.java
+++ src/main/java/candidate/Class1Exercise.java
@@ -10,27 +10,53 @@ import java.util.List;
  */
 public class Class1Exercise {
 
+    /**
+     * Iterates the list in functional way
+     * @param userList
+     */
     void printByForEach(List<User> userList){
         throw new NotImplementedException();
     }
 
+    /**
+     * Iterates list using stream interface
+     * @param userList
+     */
     void printByForEachUsingStream(List<User> userList){
         throw new NotImplementedException();
     }
 
-    List<User> filterDigit2Numbers(List<User> userList){
+    /**
+     * Filters Numbers starting with digit 2
+     * @param userList
+     * @return
+     */
+    void filterDigit2Numbers(List<User> userList){
         throw new NotImplementedException();
     }
 
-    List<User> filterUsersEndsWithEvenDigit(List<User> userList){
+    /**
+     * Filters all users with even phone number
+     * @param userList
+     * @return
+     */
+    void filterUsersEndsWithEvenDigit(List<User> userList){
         throw new NotImplementedException();
     }
 
-    List<User> filterByNameAndPhoneUsingSingleFilter(List<User> userList){
+    /**
+     * Filters users by Name and Phone
+     * @param userList
+     */
+    void filterByNameAndPhoneUsingSingleFilter(List<User> userList,String name,long phone){
         throw new NotImplementedException();
     }
 
-    List<User> filterByNameAndPhoneUsingFilterChain(List<User> userList){
+    /**
+     * Filters users by Name and Phone
+     * @param userList
+     */
+    void filterByNameAndPhoneUsingFilterChain(List<User> userList){
         throw new NotImplementedException();
     }
 }
